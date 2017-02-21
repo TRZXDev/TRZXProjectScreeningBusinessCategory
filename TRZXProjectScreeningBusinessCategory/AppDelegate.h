cf417c62a9ed216ff5cd7c1a85d5742f5f4c49ce
lifp <lifp@mmwipo.com>
2017-02-21 18:19:51 +0800

    编辑.podspec

---
+++ TRZXProjectScreeningBusinessCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TRZXProjectScreeningBusinessCategory
+//
+//  Created by N年後 on 2017/2/21.
+//  Copyright © 2017年 TRZX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
